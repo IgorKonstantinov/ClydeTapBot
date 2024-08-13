59787ec959d9390fe07344d2aac724cf5f7347cb
igor <igor.konstantinov@gmail.com>
2024-08-13 15:54:07 +0300

    remove old code

--- bot/config/config.py
+++ bot/config/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     SLEEP_LONG: list[int] = [30, 90]
     SLEEP_RANDOM: list[int] = [3, 5]
 
-    RANDOM_TAPS_COUNT: list[int] = [5, 10]
+    RANDOM_TAPS_COUNT: list[int] = [10, 19]
     SLEEP_BETWEEN_TAP: list[int] = [5, 10]     # token lifetime 60sec!
 
     APPLY_DAILY_BOOST: bool = True
